7b424c7f647d41c288a738c01e8e609a638285ac
h.ramananjato <h.ramananjato@etechconsulting-mg.com>
2023-06-19 01:01:08 +0300

    🐛 Route fix

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,26 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { authGuard } from './services/auth.guard';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-const routes: Routes =[
+const routes: Routes = [
   {
     path: '',
-    redirectTo: 'login',
-    pathMatch: 'full',
-  }, {
-    path: '/dashboard',
     component: AdminLayoutComponent,
-    canActivate: [authGuard],
+    // canActivate: [authGuard],
     children: [
       {
         path: '',
         loadChildren: () => import('src/app/layouts/admin-layout/admin-layout.module').then(m => m.AdminLayoutModule)
-      }
+      },
     ]
   }, {
     path: '',
@@ -31,9 +28,6 @@ const routes: Routes =[
         loadChildren: () => import('src/app/layouts/auth-layout/auth-layout.module').then(m => m.AuthLayoutModule)
       }
     ]
-  }, {
-    path: '**',
-    redirectTo: 'login'
   }
 ];
 
@@ -41,7 +35,7 @@ const routes: Routes =[
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
+    RouterModule.forRoot(routes, {
       useHash: true
     })
   ],
