b83789a4ab76fbec9fed861d73c2126a52cbd97c
h.ramananjato <h.ramananjato@etechconsulting-mg.com>
2023-06-20 13:36:23 +0300

    Add assignment > Implementation

--- src/app/pages/assignemnent-add/assignemnent-add.component.ts
+++ src/app/pages/assignemnent-add/assignemnent-add.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AssignmentSA } from 'src/app/services/sa/assignement.sa';
 import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
@@ -11,7 +13,18 @@ export class AssignmentAddComponent implements OnInit {
   public userInfo: any;
   public initial: any;
 
-  constructor(private storageService: StorageService) {
+
+  nom = "";
+  dateDeRendu = new Date();
+  auteur = "";
+  matiere = "";
+  note = 0;
+  remarques = "";
+
+  loading = false;
+
+
+  constructor(private storageService: StorageService, private assignmentSA: AssignmentSA, private router: Router) {
     this.userInfo = this.storageService.getUserInfo();
     this.initial = `https://ui-avatars.com/api/?name=${this.userInfo.nomprenom}&background=F5583D&color=ffffff`;
   }
@@ -19,4 +32,26 @@ export class AssignmentAddComponent implements OnInit {
   ngOnInit() {
   }
 
+
+
+  addAssignment() {
+    this.loading = true;
+    this.assignmentSA.addAssignment(this.nom, this.dateDeRendu, this.auteur, this.matiere, this.note, this.remarques).subscribe(data => {
+      console.log('this. data :>> ', data);
+      this.router.navigate(['/dashboard']);
+      this.loading = false;
+    });
+  }
+
+
+
+  onSubmit(event: any) {
+    console.log('nom :>> ', this.nom);
+    console.log('dateDeRendu :>> ', this.dateDeRendu);
+    console.log('auteur :>> ', this.auteur);
+    console.log('matiere :>> ', this.matiere);
+    console.log('note :>> ', this.note);
+    console.log('remarques :>> ', this.remarques);
+    this.addAssignment();
+  }
 }
--- src/app/services/sa/assignement.sa.ts
+++ src/app/services/sa/assignement.sa.ts
@@ -15,4 +15,19 @@ export class AssignmentSA {
     return this.apiSA.get(`/assignments?page=${page}&limit=${limit}`);
   }
 
+  addAssignment(nom: string, dateDeRendu: Date, auteur: string, matiere: string, note: number, remarques: string): Observable<any> {
+    return this.apiSA.post(`/assignments`, { nom, dateDeRendu, auteur, matiere, note, remarques });
+  }
+
+  detailAssignment(id: string): Observable<any> {
+    return this.apiSA.get(`/assignments/${id}`);
+  }
+
+  // editAssignment(id: string): Observable<any> {
+  //   return this.apiSA.put(`/assignments/${id}`);
+  // }
+
+  deleteAssignment(id: string): Observable<any> {
+    return this.apiSA.delete(`/assignments/${id}`);
+  }
 }
